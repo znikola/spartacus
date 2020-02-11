886307d1c87b81e124facb1f0cd0e34c1977ac2e
Lars Link <lars.link@sap.com>
2020-02-11 14:30:21 +0100

    Using router module for creating the url

--- projects/storefrontlib/src/cms-components/social-media/components/share-buttons/share-buttons.component.ts
+++ projects/storefrontlib/src/cms-components/social-media/components/share-buttons/share-buttons.component.ts
@@ -3,15 +3,22 @@ import {
   Component,
   OnInit,
   Inject,
+  Optional,
+  PLATFORM_ID,
 } from '@angular/core';
-import { DOCUMENT } from '@angular/common';
+import { DOCUMENT, isPlatformServer } from '@angular/common';
 import { ICON_TYPE } from '../../../misc/icon/index';
 import { ShareLinksService } from '../../share-links.service';
 import { ShareLink } from '../../share-link.model';
 import { CurrentProductService } from '../../../product/current-product.service';
-import { Product } from '@spartacus/core';
+import {
+  Product,
+  SemanticPathService,
+  SERVER_REQUEST_ORIGIN,
+} from '@spartacus/core';
 import { Observable } from 'rxjs';
 import { filter, distinctUntilChanged, switchMap } from 'rxjs/operators';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'cx-share-buttons',
@@ -29,10 +36,17 @@ export class ShareButtonsComponent implements OnInit {
     switchMap(productObject => this.getShareLinks(productObject))
   );
 
+  @Optional()
+  @Inject(SERVER_REQUEST_ORIGIN)
+  protected serverRequestOrigin?: string;
+
   constructor(
     @Inject(DOCUMENT) document,
+    @Inject(PLATFORM_ID) protected platformId,
     protected shareLinksService: ShareLinksService,
-    protected currentProductService: CurrentProductService
+    protected currentProductService: CurrentProductService,
+    private router: Router,
+    private semanticPath: SemanticPathService
   ) {
     this.document = document;
   }
@@ -41,7 +55,23 @@ export class ShareButtonsComponent implements OnInit {
     this.product$.subscribe();
   }
   private getShareLinks(product: Product): Observable<ShareLink[]> {
-    const productUrl = this.document.location.origin + '/p/' + product.code;
+    const productUrlParam = this.semanticPath.transform({
+      cxRoute: 'product',
+      params: product,
+    });
+
+    const serializedProductUrl = this.router.serializeUrl(
+      this.router.createUrlTree(productUrlParam)
+    );
+
+    let origin;
+    if (isPlatformServer(this.platformId)) {
+      origin = this.serverRequestOrigin;
+    } else {
+      origin = this.document.location.origin;
+    }
+
+    const productUrl = origin + '/' + serializedProductUrl;
 
     return this.shareLinksService.getShareLinks(productUrl, product.summary);
   }
