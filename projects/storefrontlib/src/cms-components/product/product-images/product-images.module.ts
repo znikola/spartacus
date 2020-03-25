440646fc9339001bcfc9027c3efa5e71ed325453
reyhaneh salkhi <51803127+reyhanehsalkhi@users.noreply.github.com>
2020-03-25 11:19:25 -0400

    Image Zoom for product detail page

--- projects/storefrontlib/src/cms-components/product/product-images/product-images.module.ts
+++ projects/storefrontlib/src/cms-components/product/product-images/product-images.module.ts
@@ -6,6 +6,7 @@ import { OutletModule } from '../../../cms-structure/outlet/index';
 import { CarouselModule } from '../../../shared/components/carousel/index';
 import { MediaModule } from '../../../shared/components/media/media.module';
 import { ProductImagesComponent } from './product-images.component';
+import { AppZoomModule } from '../image-zoom/app-zoom.module';
 
 @NgModule({
   imports: [
@@ -14,6 +15,7 @@ import { ProductImagesComponent } from './product-images.component';
     MediaModule,
     OutletModule,
     CarouselModule,
+    AppZoomModule,
   ],
   providers: [
     provideDefaultConfig(<CmsConfig>{
