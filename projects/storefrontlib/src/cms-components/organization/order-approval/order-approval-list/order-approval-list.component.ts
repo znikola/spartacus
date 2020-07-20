2dfbd2b612722e9f527308fa774ea52670d55ef6
qjimmy <quang-jimmy@hotmail.com>
2020-07-20 14:19:53 -0400

    translation set

--- projects/storefrontlib/src/cms-components/organization/order-approval/order-approval-list/order-approval-list.component.ts
+++ projects/storefrontlib/src/cms-components/organization/order-approval/order-approval-list/order-approval-list.component.ts
@@ -6,13 +6,11 @@ import {
   OrderApproval,
   OrderApprovalService,
   RoutingService,
+  TranslationService,
 } from '@spartacus/core';
 import { Observable } from 'rxjs';
 import { filter, map, switchMap, tap } from 'rxjs/operators';
-import {
-  AbstractListingComponent,
-  ListingModel,
-} from '../../abstract-component/abstract-listing.component';
+import { AbstractListingComponent } from '../../abstract-component/abstract-listing.component';
 
 @Component({
   selector: 'cx-order-approval-list',
@@ -23,33 +21,47 @@ export class OrderApprovalListComponent extends AbstractListingComponent
   constructor(
     protected routingService: RoutingService,
     protected orderApprovalService: OrderApprovalService,
+    protected translation: TranslationService,
     protected cxDate: CxDatePipe
   ) {
     super(routingService);
   }
 
   ngOnInit(): void {
-    this.data$ = <Observable<ListingModel>>this.queryParams$.pipe(
+    this.data$ = <Observable<any>>this.queryParams$.pipe(
       tap((queryParams: B2BSearchConfig): void =>
         this.orderApprovalService.loadOrderApprovals(queryParams)
       ),
       switchMap((queryParams: B2BSearchConfig) =>
         this.orderApprovalService.getList(queryParams).pipe(
           filter(Boolean),
-          map((orderApprovalList: EntitiesModel<OrderApproval>) => ({
-            sorts: orderApprovalList.sorts,
-            pagination: orderApprovalList.pagination,
-            values: orderApprovalList.values.map(
-              (orderApproval: OrderApproval) => ({
-                approvalCode: orderApproval.code,
-                POCode: orderApproval.order.purchaseOrderNumber,
-                placedBy: `${orderApproval.order.orgCustomer.name} ${orderApproval.order.orgCustomer.orgUnit.name}`,
-                date: this.cxDate.transform(orderApproval.order.created),
-                status: `${orderApproval.order.statusDisplay}`,
-                total: orderApproval.order.totalPriceWithTax,
-              })
-            ),
-          }))
+          map((orderApprovalList: EntitiesModel<OrderApproval>) => {
+            return {
+              sorts: orderApprovalList.sorts,
+              pagination: orderApprovalList.pagination,
+              values: orderApprovalList.values.map(
+                (orderApproval: OrderApproval) => {
+                  let status;
+                  this.translation
+                    .translate(
+                      `orderDetails.statusDisplay_${orderApproval.order.statusDisplay}`
+                    )
+                    .subscribe((order_status) => {
+                      status = order_status;
+                    });
+
+                  return {
+                    approvalCode: orderApproval.code,
+                    POCode: orderApproval.order.purchaseOrderNumber,
+                    placedBy: `${orderApproval.order.orgCustomer.name} ${orderApproval.order.orgCustomer.orgUnit.name}`,
+                    date: this.cxDate.transform(orderApproval.order.created),
+                    status,
+                    total: orderApproval.order.totalPrice.formattedValue,
+                  };
+                }
+              ),
+            };
+          })
         )
       )
     );
